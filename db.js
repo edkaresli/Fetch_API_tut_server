a8dfc585c11cbf9fe9be1780e86d94c2260ced40
Karesli <eduard.karesli@accenture.com>
2019-03-26 16:02:43 +0100

    Adding some DB connectivity and data models

--- db.js
+++ db.js
@@ -1,7 +1,13 @@
+const Sequelize = require('sequelize');
+const dbconfig = require('./dbconfig');
 
+const uri = dbconfig.POSTGRES_HOST_URI;
+const port = dbconfig.POSTGRES_HOST_PORT;
+const user = dbconfig.POSTGRES_USER;
+const password = dbconfig.POSTGRES_PASSWORD;
+const dbname = dbconfig.POSTGRES_DB_NAME;
 
-class DBOperations {
-    
-}
+const sequelize = new Sequelize(`postgres://${user}:${password}@${uri}:${port}/${dbname}`);
+const DB = {Sequelize, sequelize};
 
-module.exports = DBOperations;
+module.exports = DB;
